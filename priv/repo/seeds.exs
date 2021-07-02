40babe4f227487bf2b579d0e6c99c042f01bd762
Dev Wasi <wasibiit@gmail.com>
2021-07-02 23:26:07 +0500

    added seeds for programs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -20,7 +20,12 @@ DApp.Repo.insert(%DApp.Schema.User{first_name: "Nadia", last_name: "Tariq", dob:
 DApp.Repo.insert(%DApp.Schema.User{first_name: "Api", last_name: "Saeeda", dob: ~D[1999-12-16], email: "apisaeedat@ls.com", password: Argon2.hash_pwd_salt("123"), role_id: "teacher"})
 DApp.Repo.insert(%DApp.Schema.User{first_name: "Awais", last_name: "Khalid", dob: ~D[1996-01-12], email: "awaiskhaild@gmail.com", password: Argon2.hash_pwd_salt("123"), role_id: "student"})
 DApp.Repo.insert(%DApp.Schema.User{first_name: "M", last_name: "Touseef", dob: ~D[1995-02-16], email: "mtouseef@gmail.com", password: Argon2.hash_pwd_salt("123"), role_id: "student"})
-DApp.Repo.insert(%DApp.Schema.User{first_name: "M", last_name: "Shexi", dob: ~D[1994-05-02], email: "mshexi@gmail.com", password: Argon2.hash_pwd_salt("123"), role_id: "student"})
+DApp.Repo.insert(%DApp.Schema.User{first_name: "John", last_name: "Doe", dob: ~D[1994-05-02], email: "john@doe.com", password: Argon2.hash_pwd_salt("123"), role_id: "student"})
+
+DApp.Repo.insert(%DApp.Schema.Program{id: "bscs", duration: "4 years"})
+DApp.Repo.insert(%DApp.Schema.Program{id: "bsit", duration: "4 years"})
+DApp.Repo.insert(%DApp.Schema.Program{id: "bsurdu", duration: "4 years"})
+DApp.Repo.insert(%DApp.Schema.Program{id: "bsmath", duration: "4 years"})
 
 #DApp.Repo.insert(%DApp.Schema.Question{question: "What Would You Drink?", options: %{"a" => "Cold Coffee", "b" => "Hot Coffee"}})
 #DApp.Repo.insert(%DApp.Schema.Question{question: "What Would You Choose?", options: %{"a" => "Rose", "b" => "Sun Flower", "c" => "Lily", "d" => "Iris"}})
